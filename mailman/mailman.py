fe348bd73a8dfd11d367a0d940eff991e284c661
Alec Burgdorf <aeburgd@gmail.com>
2017-06-13 19:19:24 -0500

    add roll_check to mailman implementation

--- mailman/mailman.py
+++ mailman/mailman.py
@@ -1,8 +1,10 @@
 from player import PlayerMailman
+from util import roll_check
 
 
 mojave = PlayerMailman()
 mojave_special = mojave.assign_attributes()
+mojave_special = roll_check(mojave_special)
 mojave_skills = mojave.skill_tag()
 mojave_traits = mojave.traits_pick()
 
