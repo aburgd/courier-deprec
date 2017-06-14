95c0237ee9dcd94af2d338ba414ee5230bd30d76
Alec Burgdorf <aeburgd@gmail.com>
2017-06-13 21:12:42 -0500

    update implementation to reflect changes

--- courier/courier.py
+++ courier/courier.py
@@ -3,12 +3,16 @@ from util import roll_check
 
 
 mojave = PlayerCourier()
+name_gender = mojave.name_gender()
+mojave_name = mojave.tell_me_your_name(name_gender)
+mojave_gender = mojave.display_gender()
 mojave_special = mojave.assign_attributes()
 mojave_special = roll_check(mojave_special)
 mojave_skills = mojave.skill_tag()
 mojave_traits = mojave.traits_pick()
 
-print("Your Courier")
+print("Name: {}".format(mojave_name))
+print("Gender: {}".format(mojave_gender))
 print("SPECIAL: {}".format(mojave_special))
 print("Skills: {}".format(mojave_skills))
 print("Traits: {}".format(mojave_traits))
