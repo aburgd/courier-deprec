612478244217aab544c94120b314fb6f727908cf
Alec Burgdorf <aeburgd@gmail.com>
2017-06-13 19:19:44 -0500

    remove newline from first print

--- mailman/mailman.py
+++ mailman/mailman.py
@@ -8,7 +8,7 @@ mojave_special = roll_check(mojave_special)
 mojave_skills = mojave.skill_tag()
 mojave_traits = mojave.traits_pick()
 
-print("Your Courier\n")
+print("Your Courier")
 print("SPECIAL: {}".format(mojave_special))
 print("Skills: {}".format(mojave_skills))
 print("Traits: {}".format(mojave_traits))
